8a4efe363ec1799d259a7b790f004caf7dd6d53c
ebookineur <pierre@ebookineur.com>
2012-01-18 09:57:44 -0800

    code block parsing

---
+++ src/java/com/ebookineur/markdown/impl/scanner/BlockCode.java
@@ -0,0 +1,39 @@
+package com.ebookineur.markdown.impl.scanner;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ebookineur.markdown.MarkdownRenderer;
+
+public class BlockCode extends BlockElement {
+
+	int _currentLevel;
+
+	public BlockCode(MdParser parser, MdOutput output) {
+		super(parser, output);
+	}
+
+	public void render(MarkdownRenderer renderer, DocumentInformation di)
+			throws IOException {
+		// we need to remove any leading 4 spaces or \t from
+		// each line before rendering the code
+		List<String> lines = new ArrayList<String>();
+
+		for (String line : _lines) {
+			if (line.length() > 0) {
+				if (line.charAt(0) == '\t') {
+					lines.add(line.substring(1));
+				} else {
+					lines.add(line.substring(4));
+				}
+			} else {
+				lines.add(line);
+			}
+		}
+		String result = renderer.code(lines);
+
+		_output.println(result);
+	}
+
+}
